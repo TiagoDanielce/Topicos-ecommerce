7c02be101769eb821634aa876b0e690584b3d6ea
Jeferson Schneider <jefer.02@hotmail.com>
2019-05-01 23:18:06 -0300

    Criação da tela Index e também de Edição

--- Topicos/Controllers/UsuarioController.cs
+++ Topicos/Controllers/UsuarioController.cs
@@ -20,5 +20,41 @@ namespace Topicos.Controllers
             var list = db.UsuariosDB.Find(p => true).ToList();
             return View(list);
         }
+
+        // GET
+        public ActionResult Edit(string id)
+        {
+            ViewBag.Admin = true;
+            ViewBag.ExibeFooter = false;
+            var usuario = db.UsuariosDB.Find(p => p.Id == id).FirstOrDefault();
+            if (!string.IsNullOrEmpty(id))
+            {
+                return View(usuario);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(string id, UsuarioModel model)
+        {
+            ViewBag.Admin = true;
+            ViewBag.ExibeFooter = false;
+
+            if (ModelState.IsValid)
+            {
+                if (!string.IsNullOrEmpty(id))
+                {
+                    model.Id = id;
+                    var filter = Builders<UsuarioModel>.Filter.Eq(p => p.Id, id);
+                    db.UsuariosDB.ReplaceOne(filter, model);
+                }
+                else
+                {
+                    db.UsuariosDB.InsertOne(model);
+                }
+            }
+            return View(model);
+        }
     }
 }
